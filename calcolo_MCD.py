91cacd430caa228fdc2b8381b1c8e149238d7bb6
Renzetti_Alessio <alessiorenzetti2004@gmail.com>
2024-02-13 20:47:35 +0100

    final commit

--- calcolo_MCD.py
+++ calcolo_MCD.py
@@ -5,6 +5,6 @@ def calcola_MCD(n1,n2):
         return calcola_MCD(n1,n2-n1)
     else:
         return calcola_MCD(n2,n1-n2)
-n1=27
-n2=45
+n1=int(input("dimmi il primo numero "))
+n2=int(input("dimmi il secondo numero "))
 print(calcola_MCD(n1,n2))
