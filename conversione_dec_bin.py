c3863cd8b4a12fdc7e0cdcd29dfe967873af28ca
Alessio-Renzetti <alessiorenzetti2004@gmail.com>
2024-02-13 14:54:28 +0100

    Es_python

--- conversione_dec_bin.py
+++ conversione_dec_bin.py
@@ -1,10 +1,7 @@
-
 def conversione(n):
     if n!=0:
        return str(conversione(n//2))+str(n%2)
     else:
         return ""
-n=int(input("che numero vuoi convertire in binario? ")) 
-app=conversione(n)   
-n_binario=""
-print("il numero in binario è",app)
+n=int(input("che numero vuoi convertire in binario? "))    
+print("il numero in binario è",conversione(n))
