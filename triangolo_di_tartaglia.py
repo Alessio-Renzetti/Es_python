1d0c3a50e7ba57736d9effde3e1ba80a128c7641
Alessio-Renzetti <alessiorenzetti2004@gmail.com>
2024-02-13 16:28:49 +0100

    final commit

--- triangolo_di_tartaglia.py
+++ triangolo_di_tartaglia.py
@@ -9,7 +9,11 @@ def trova_num(n,k):
         return trova_num(n-1,k-1)+trova_num(n-1,k)
 n=int(input("In che riga vuoi cercare il numero "))
 k=int(input("In che colonna vuoi cercare il numero "))
-print("Il numero che cerchi è",trova_num(n,k))
+if k>n:
+    k=int(input("hai inserito un numero di colonne errato inseriscine uno corretto "))
+    print("Il numero che cerchi è",trova_num(n,k))
+else:
+    print("Il numero che cerchi è",trova_num(n,k))
 #1
 #1 1
 #1 2 1
