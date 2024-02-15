36471996671a7dd10e64a0a835eb48d33a889341
Alessio-Renzetti <alessiorenzetti2004@gmail.com>
2024-02-15 16:42:39 +0100

    final commit

--- ordina_alfabetico.py
+++ ordina_alfabetico.py
@@ -1,11 +1,11 @@
 def ordine_alfabetico(parola1,parola2):
     if len(parola1)!=0 and len(parola2)!=0: 
         if parola1[0]>parola2[0]:
-            return parola2[0]+ordine_alfabetico(parola1.replace(parola1[0],""),parola2.replace(parola2[0],""))
+            return parola2[0]+ordine_alfabetico(parola1[+1 :],parola2[+1 :])
         elif parola1[0]<parola2[0]:
-            return parola1[0]+ordine_alfabetico(parola1.replace(parola1[0],""),parola2.replace(parola2[0],""))
+            return parola1[0]+ordine_alfabetico(parola1[+1 :],parola2[+1 :])
         else:
-            return parola1[0]+ordine_alfabetico(parola1.replace(parola1[0],""),parola2.replace(parola2[0],""))
+            return parola1[0]+ordine_alfabetico(parola1[+1 :],parola2[+1 :])
     else:
         return ""
 parola1=str(input("digita una parola "))
