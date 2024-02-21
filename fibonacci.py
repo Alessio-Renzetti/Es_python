7e0f66061fc160101f5975ce3db0a64a07371e97
Alessio-Renzetti <alessiorenzetti2004@gmail.com>
2024-02-21 15:52:51 +0100

    final commit

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,13 @@
+def sez_aurea(n,app):
+    if n>1:
+        return sez_aurea(n-1,app)+sez_aurea(n-2,app)
+    elif n==0:
+        return 0
+    else:
+        return 1/seq_fibonacci(app)
+
+
+
 def seq_fibonacci(n):
     if n>1:
         return seq_fibonacci(n-1)+seq_fibonacci(n-2)
@@ -7,5 +17,6 @@ def seq_fibonacci(n):
         return 1
 
 n=int(input("in che posizione vuoi cercare il numero? "))
-print("il numero che cercavi è ",seq_fibonacci(n))
-print("la sezione aurea è ",seq_fibonacci(n)/seq_fibonacci(n-1))    
+print("il numero che cercavi è",seq_fibonacci(n))
+print("la sezione aurea è",sez_aurea(n,n-1))
+    
